0058b9854e596641cff638e8ffa843229460d95e
wl02008923 <wl02008923@gmail.com>
2018-12-04 23:33:52 +0800

    2018-12-04 23:33

--- pages/js/deal.js
+++ pages/js/deal.js
@@ -1,25 +1,8 @@
-$(".delete").on("click",function(){
-  $(this).parent().parent().remove();
-})
-
-var td1 = "";
-var td2 = "";
-$(".edit").on("click",function(){
-  td1 = $(this).parent().prev().prev().text();
-  $(this).parent().prev().prev().text("");
-  $(this).parent().prev().prev().html("<input value='"+td1+"'>");
-  td2 = $(this).parent().prev().text();
-  $(this).parent().prev().text("");
-  $(this).parent().prev().html("<input value='"+td2+"'>");
-  $(this).parent().html("<button class='conform'>確認</button>");
-  $(".conform").on("click",function(){
-    location.reload();
-  })
-})
 
 $("#dialog1").css("display","none");
 $("#msg1").css("display","none");
 $("#dialog1").css("opacity","0.5");
+
 
 function closeDialog1(){
 $("#dialog1").css("display","none");
