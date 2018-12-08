757fc212856f3fdd0c850bd5c9e6ace271b87c12
羅靖婷 <tinaluo2945@gmail.com>
2018-12-08 11:05:28 +0800

    index

---
+++ pages/indexaction.php
@@ -0,0 +1,41 @@
+<?php
+    $db = mysqli_connect("localhost","root","root") or die("無法開啟MySQL伺服器連接!");
+    $dbname = "Gastation";
+    if (!mysqli_select_db($db,$dbname)) {
+        die("無法開啟$dbname資料庫");
+    }
+if (isset($_POST['save'])){
+        $name=$_POST['Name'];
+        $gender=$_POST['Gender'];
+        $Personal_ID=$_POST['Personal_ID'];
+        $Birthday=$_POST['Birthday'];
+        $Salary=$_POST['Salary'];
+        $Station_ID=$_POST['station_id'];
+        
+        
+        
+        mysqli_query($db,"INSERT INTO Staff(Name,Gender,Personal_ID,Birthday,Station_ID) VALUES ('$name', $gender, '$Personal_ID','$Birthday','$Station_ID');");
+        mysqli_query($db,"INSERT INTO Fulltime VALUES(last_insert_id(),'$Salary');");
+        echo "<meta http-equiv=REFRESH CONTENT=2;url=index.php>";
+        
+    }
+ if(isset($_GET['delete'])){
+ 	mysqli_query($db,"DELETE FROM Staff WHERE Personal_ID = '$_GET[Personal_ID]'");
+ 	echo "<meta http-equiv=REFRESH CONTENT=2;url=index.php>";
+ }
+   //echo "<meta http-equiv=REFRESH CONTENT=2;url=index.php>";
+if (isset($_POST['edit'])){
+        $fullid = $_POST['fullid'];
+        $salary=$_POST['salary'];
+        $station_id=$_POST['Station_id'];
+        $id=$_POST['id'];
+		// echo "<script type='text/javascript'>alert('$station_id');</script>";
+         mysqli_query($db,"UPDATE Fulltime SET Salary = '$salary' WHERE Staff_ID =$id;");
+         mysqli_query($db,"UPDATE Staff SET Station_ID = '$station_id' WHERE Staff_ID = $id;");
+        echo "<meta http-equiv=REFRESH CONTENT=2;url=index.php>";
+        // UPDATE `Staff` SET `Station_ID` = '1' WHERE `Staff`.`Staff_ID` = 32;
+        
+    }
+
+
+?>
--- pages/js/index.js
+++ pages/js/index.js
@@ -1,16 +1,21 @@
 $(".delete").on("click",function(){
   $(this).parent().parent().remove();
 })
+var test = "$result3" ;
+var options = "<select name='station_id'><?phpfor($i=1;$i<=mysqli_num_rows($result3);$i++){$rs3=mysqli_fetch_row($result3);?><option value='<?php echo $rs3[1]?>'><?php echo $rs3[0]?></option><?php}?></select>"
+
+//var station = $("#station").val();
 
 var td1 = "";
 var td2 = "";
+var array=[1,2,3,4];
 $(".edit").on("click",function(){
   td1 = $(this).parent().prev().prev().text();
   $(this).parent().prev().prev().text("");
-  $(this).parent().prev().prev().html("<input value='"+td1+"'>");
+  $(this).parent().prev().prev().html("<input value='"+td1+"' name='Salary'>");
   td2 = $(this).parent().prev().text();
   $(this).parent().prev().text("");
-  $(this).parent().prev().html("<input value='"+td2+"'>");
+  $(this).parent().prev().html(options);
   $(this).parent().html("<button class='conform'>確認</button>");
   $(".conform").on("click",function(){
     location.reload();
@@ -41,4 +46,3 @@ var _left=_sw/2-w/2;
 $("#msg").css("top",_top+'px');
 $("#msg").css("left",_left+'px');
 }
-
