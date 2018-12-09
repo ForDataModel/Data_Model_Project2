470fdc26b6e4b2ed5d1e4bfeeccf4712eee3e6e8
1.6.3_Tommy <>
2018-12-09 20:56:49 +0800

    commit php

--- pages/confirmNew.php
+++ pages/confirmNew.php
@@ -1,24 +1,27 @@
 <?php
+    $db = mysqli_connect("localhost","root","root","Gastation") or die("無法開啟MySQL伺服器連接!");
+    if (mysqli_connect_errno()) {
+        die("無法開啟$dbname資料庫");
+    }
+
+    mysqli_begin_transaction($db, MYSQLI_TRANS_START_READ_WRITE);
+
     $station_name = $_POST['station_name'];
     $station_address = $_POST['station_address'];
     $station_phone = $_POST['station_phone'];
     $ManagerID = $_POST["Staff_ID"];
-    $db = mysqli_connect("localhost","root","root") or die("無法開啟MySQL伺服器連接!");
-    $dbname = "Gastation";
-    if (!mysqli_select_db($db,$dbname)) {
-        die("無法開啟$dbname資料庫");
-    }
+
     $sqlInsertStation = "INSERT INTO Station (Station_ID, Address, Name, Manager_ID, Daily_revenue, Phone_number, Oil_Supplier_ID, Product_Supplier_ID) 
                               VALUES (NULL, '$station_address', '$station_name', '$ManagerID', NULL, '$station_phone', NULL, NULL)";
     $resultInsertStation = mysqli_query($db,$sqlInsertStation);
     $sqlStationID = "SELECT Station_ID FROM Station WHERE Name = $station_name AND Address = $station_address";
     $rowStationID = mysqli_fetch_array(mysqli_query($db,$sqlStationID));
     $StationID  = $rowStationID['Station_ID'];
-     $sqlUpdateStaff =  "UPDATE Staff SET Station_ID	=  '$StationID' WHERE Staff_ID = '$ManagerID'";
+     $sqlUpdateStaff =  "UPDATE Staff SET Station_ID = '$StationID' WHERE Staff_ID = '$ManagerID'";
     $resultUpdateStaff = mysqli_query($db,$sqlUpdateStaff);
-    mysqli_commit($db);
 
     if($resultInsertStation){
+        mysqli_commit($db);
         echo '<h2><b>資料送出成功!</b></h2>';
         echo "<meta http-equiv=REFRESH CONTENT=2;url=station.php>";
     }else{
--- pages/updateCommodity.php
+++ pages/updateCommodity.php
@@ -10,22 +10,12 @@
         $Oil_amount = $_POST['Oil_amount'];
         $station_ID = $_POST['station_ID'];
 
-        mysqli_begin_transaction($db, MYSQLI_TRANS_START_READ_WRITE);
 
         $sqlInsertOil = "INSERT INTO Oil (Oil_ID, Name, Oil_cost, Oil_price, Oil_amount, Station_ID, Date) 
                               VALUES (NULL, '$Oil_Name', '$Oil_cost', NULL, '$Oil_amount', '$station_ID', CURRENT_TIMESTAMP)";
         $resultInsertOil = mysqli_query($db,$sqlInsertOil);
 
-        $sql = "SELECT oil_Total_Amount 
-                  FROM storage 
-                 WHERE oil_Name = '$Oil_Name' AND station_ID = '$station_ID'";
-        $rowAmount = mysqli_fetch_array(mysqli_query($db,$sql));
-        $oil_Total_Amount  = $rowAmount['oil_Total_Amount']+$Oil_amount;
-        $sqlUpdateStorage = "UPDATE storage SET oil_Total_Amount = '$oil_Total_Amount' WHERE oil_Name = '$Oil_Name' AND station_ID = '$station_ID'";
-        $resultUpdateStorage = mysqli_query($db,$sqlUpdateStorage);
-
-        if($resultInsertOil && $resultUpdateStorage){
-            mysqli_commit($db);
+        if($resultInsertOil){
             echo '<h2><b>資料送出成功!</b></h2>';
             echo "<meta http-equiv=REFRESH CONTENT=2;url=storage.php>";
         }else{
@@ -45,23 +35,11 @@
         $Product_amount = $_POST['Product_amount'];
         $station_ID2 = $_POST['station_ID2'];
 
-        mysqli_begin_transaction($db, MYSQLI_TRANS_START_READ_WRITE);
-
         $sqlInsertProduct = "INSERT INTO Product (Product_ID, Product_name, Price, Product_amount, Cost, Product_Date, Station_ID) 
                                   VALUES (NULL, '$Product_Name', NULL, '$Product_amount', '$Product_cost', CURRENT_TIMESTAMP, '$station_ID2')";
         $resultInsertProduct = mysqli_query($db,$sqlInsertProduct);
-        $sqlgoods = "SELECT Product_total_amount 
-                       FROM Goods  
-                      WHERE Product_name = '$Product_Name' AND station_ID = '$station_ID2'";
-        $rowtotal = mysqli_fetch_array(mysqli_query($db,$sqlgoods));
-        $product_Total_Amount  = $rowtotal['Product_total_amount']+ $Product_amount;
-        $sqlUpdateGoods = "UPDATE Goods 
-                              SET Product_total_amount = '$product_Total_Amount' 
-                            WHERE Product_name = '$Product_Name' AND station_ID = '$station_ID2'";
-        $resultUpdateGoods = mysqli_query($db,$sqlUpdateGoods);
-
-        if($resultInsertProduct && $resultUpdateGoods){
-            mysqli_commit($db);
+       
+        if($resultInsertProduct ){
             echo '<h2><b>資料送出成功!</b></h2>';
             echo "<meta http-equiv=REFRESH CONTENT=2;url=storage.php>";
         }else{
